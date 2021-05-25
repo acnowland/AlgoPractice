451b47d9ea8c4b87079807c9ca8c9f8622fdcfa8
Adam Nowland <acnowland@gmail.com>
2021-05-25 12:47:25 -0600

    algos

--- index.js
+++ index.js
@@ -16,19 +16,51 @@
 // }
 // sameFrequency(110,101)
 
-function areThereDuplicates(...variables){
-    let duplicateCheck = {}
-    for(var i = 0 ; i<variables.length;i++){
-        let currentItem = variables[i]
-        if(duplicateCheck[currentItem]){
-            return true
-        }else{
-            duplicateCheck[currentItem] = 1
+// function areThereDuplicates(...variables){
+//     let duplicateCheck = {}
+//     for(var i = 0 ; i<variables.length;i++){
+//         let currentItem = variables[i]
+//         if(duplicateCheck[currentItem]){
+//             return true
+//         }else{
+//             duplicateCheck[currentItem] = 1
+//         }
+//     }
+//     return false
+// }
+
+// areThereDuplicates(1,2,3,4,5)
+
+// function averagePair(numberArray, average){
+//     let start = 0 
+//     let end = numberArray.length - 1
+//     while(start < end){
+//         let currentAverage = (numberArray[start] + numberArray[end]) / 2
+//         if(currentAverage === average){
+//             console.log(true)
+//             return
+//         }else if(currentAverage > average){
+//             end--
+//         }else{
+//             start++
+//         }
+//     }
+//     console.log(false)
+//     return false
+// }
+
+// averagePair([1,3,5], 2.5)
+
+function isSubsequence(firstString, secondString){
+    let firstStringLocation = 0 
+    for(var i = 0; i<secondString.length;i++){
+        let currentLetter = secondString[i]
+        if(currentLetter == firstString[firstStringLocation]){
+            firstStringLocation++ 
         }
     }
-    return false
+    return(firstStringLocation == firstString.length)
+
 }
 
-areThereDuplicates(1,2,3,4,5)
-
-
+console.log(isSubsequence('hellp', 'hello world'))
