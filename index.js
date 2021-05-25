c0872255518c32b839af4d881805b785059a6203
Adam Nowland <acnowland@gmail.com>
2021-05-25 11:43:04 -0600

    more algos

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-console.log('hello')
 
+// function sameFrequency(num1, num2){
+//     const num1Split = num1.toString().split('')
+//     const num2Split = num2.toString().split('')
+//     let freqNum1= {}
+//     let freqNum2 = {}
+//     for(var i = 0;i<num1Split.length;i++){
+//         let currentNum = num1Split[i]
+//         freqNum1[currentNum] ? freqNum1[currentNum] += 1 : freqNum1[currentNum] = 1
+//     }
+//     for(var i = 0;i<num2Split.length;i++){
+//         let currentNum = num2Split[i]
+//         freqNum2[currentNum] ? freqNum2[currentNum] += 1 : freqNum2[currentNum] = 1
+//     }
+//     return(JSON.stringify(freqNum1) === JSON.stringify(freqNum2))
+// }
+// sameFrequency(110,101)
 
-function sameFrequency(num1, num2){
-    const num1Split = num1.toString().split('')
-    const num2Split = num2.toString().split('')
-    let freqNum1= {}
-    let freqNum2 = {}
-    for(var i = 0;i<num1Split.length;i++){
-        let currentNum = num1Split[i]
-        freqNum1[currentNum] ? freqNum1[currentNum] += 1 : freqNum1[currentNum] = 1
+function areThereDuplicates(...variables){
+    let duplicateCheck = {}
+    for(var i = 0 ; i<variables.length;i++){
+        let currentItem = variables[i]
+        if(duplicateCheck[currentItem]){
+            return true
+        }else{
+            duplicateCheck[currentItem] = 1
+        }
     }
-    for(var i = 0;i<num2Split.length;i++){
-        let currentNum = num2Split[i]
-        freqNum2[currentNum] ? freqNum2[currentNum] += 1 : freqNum2[currentNum] = 1
-    }
-    return(JSON.stringify(freqNum1) === JSON.stringify(freqNum2))
+    return false
 }
 
-sameFrequency(110,101)
+areThereDuplicates(1,2,3,4,5)
+
+
